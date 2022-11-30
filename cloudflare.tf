2867ce97063a9c47e261461276df75d933738145
Cleiton Kiper <cleitonahnerth@gmail.com>
2022-11-29 21:23:16 -0400

    feat: add terraform

---
+++ cloudflare.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 3.0"
+    }
+  }
+}
+
+variable "cloudflare_api_token" {
+  description = "The cloudflare API TOKEN"
+  type        = string
+}
+
+provider "cloudflare" {
+  api_token = var.cloudflare_api_token
+}
+
+
+resource "cloudflare_pages_project" "basic_project" {
+  account_id = "a134f5c96e90e0c3fec81a95dc00725e"
+  name       = "terraform-teste"
+  production_branch = "main"
+}
